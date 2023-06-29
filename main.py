d0c498827043a1b8e8f6c6c809267c8862c7ab21
Danny <cool.huip@gmail.com>
2023-06-29 01:02:46 -0400

    Finish get_slot_machine_spin()

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
+import random
+
+
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    """
+    "Which symbol will represent each column, based on the frequency of
+    our symbols we've defined outside of this function?"
+    """
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        curr_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(curr_symbols)
+            curr_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    
+    return columns
 
 
 def deposit():
@@ -13,12 +52,12 @@ def deposit():
                 print("Amount must be greater than 0.")
         else:
             print("Please enter a number.")
-
     return amount
+
 
 def get_number_of_lines():
     while True:
-        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + "): ")
+        lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}): ")
         if lines.isdigit():
             lines = int(lines)
             if (1 <= lines <= MAX_LINES):
@@ -31,9 +70,34 @@ def get_number_of_lines():
     return lines
 
 
+def get_bet():
+    while True:
+        amount = input("How much would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if (MIN_BET <= amount <= MAX_BET):
+                break    # Break out of the while-loop
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
 
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        if (total_bet > lines):
+            print(f"You cannot bet more than your balance.\nYour current balance: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines} lines. Total bet: ${total_bet}")
 
 
 main()
